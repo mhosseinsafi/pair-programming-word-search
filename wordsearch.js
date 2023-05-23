41f6d9d2a942ae9292d51f748eb39bbb0de7879d
Hossein <hossein.saafi@gmail.com>
2023-05-22 18:08:31 -0700

    Pair Programming - Word Search

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,27 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
-    }
-}
 
-module.exports = wordSearch
+// Pair Programming - Word Search done with @mhosseinsafi and @s-jesse
+
+const wordSearch = (letters, word) => {
+    const horizontalJoin = letters.map(ls => ls.join(''));
+    for (const l of horizontalJoin) {
+      if (l.includes(word)) {
+        return true;
+      }
+    }
+    const verticalJoin = [];
+    for (let i = 0; i < letters[0].length; i++) {
+      let vertStr = "";
+      for (let j = 0; j < letters.length; j++) {
+        vertStr += letters[j][i];
+      }
+      verticalJoin.push(vertStr);
+    }
+    for (const l of verticalJoin) {
+      if (l.includes(word)) {
+        return true;
+      }
+    }
+    return false;
+  };
+  
+  module.exports = wordSearch;
